129ae788df0f257154d8036c686f7fa1cca6e537
Amin Mousavi <tigmin@gmail.com>
2020-04-23 22:43:17 +0200

    Update git installer, remove .env file, add global gitignore

--- installers/git.sh
+++ installers/git.sh
@@ -1,7 +1,11 @@
+#!/usr/bin/env bash
 function install_git(){
   log "Installing 'git'..."
   brew_install "git"
 
   log "Installing 'ctags'..."
   brew_install "ctags"
+
+	echo "Set a global ignore file for git"
+  git config --global core.excludesfile ./config/.gitignore_global
 }
